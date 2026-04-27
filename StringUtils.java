a388dec5bd432d86c06c4110a21a718520e64be5
Rahul Vishwakarma <rahulvishwakarma@Rahuls-MacBook-Pro.local>
2026-04-27 15:32:10 +0530

    Modifies StringUtils class

--- StringUtils.java
+++ StringUtils.java
@@ -2,10 +2,7 @@
 class StringUtils {
   static boolean containsChar(String str, char letter){
     int index = str.indexOf(letter);
-    if(index != -1){
-      return true;
-    }
-    return false;
+    return index != -1;
   }
   
   static String reverse(String str){
@@ -20,14 +17,11 @@ class StringUtils {
   
   static String replaceAll(String str, char from, char to){
     String replaced = "";
-    int length = str.length();
 
-    for(int i = 0; i < length; i++){
-      if(str.charAt(i) == from){
-        replaced += to;
-      } else{
-        replaced += str.charAt(i);
-      }
+    for(int i = 0; i < str.length(); i++){
+      char charAt = str.charAt(i);
+      char letter = charAt == from ? to : charAt;
+      replaced += letter;
     }
     return replaced;
   }
